cf55c5da045caf76c39ec359ac4d95c668123965
donatellony <donatellony4736@gmail.com>
2024-01-13 14:20:49 +0100

    Created a basic consumer from SQS

---
+++ AwsCourse/AwsCourse/Sqs.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = ["All"], // NOT RECOMMENDED TO LEAVE "ALL". It is there for the example purposes only!
+    MessageAttributeNames = ["All"] // NOT RECOMMENDED TO LEAVE "ALL". It is there for the example purposes only!
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Body: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+    await Task.Delay(3000);
+}
