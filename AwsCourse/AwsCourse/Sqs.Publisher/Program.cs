7fdfadfe0d9524445c8a52f45befd5ba7a7f2a8a
donatellony <donatellony4736@gmail.com>
2024-01-13 14:06:28 +0100

    Created the basic publisher to SQS

---
+++ AwsCourse/AwsCourse/Sqs.Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using Sqs.Contracts;
+
+var sqsClient = new AmazonSQSClient();
+
+var customerCreated = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    FullName = "Yehor Voiko",
+    Email = "yehorvoiko@example123123123.com",
+    GitHubUsername = "yehorvoiko",
+    DateOfBirth = new DateTime(2000, 1, 1)
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customerCreated),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
