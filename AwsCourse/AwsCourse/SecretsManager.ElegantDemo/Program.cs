37a4bbdbde9102ded1a85deed6ff0b8682315c5d
donatellony <donatellony4736@gmail.com>
2024-01-14 17:38:53 +0100

    Added the more elegant Secrets Manager Demo

---
+++ AwsCourse/AwsCourse/SecretsManager.ElegantDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+var env = builder.Environment.EnvironmentName;
+var appName = builder.Environment.ApplicationName;
+
+// Secret name in the Secret Manager should look like:
+// Development_SecretsManager.ElegantDemo_OuterConfigSection__InnerConfigSection__InnerOfTheInnerConfigSection
+builder.Configuration.AddSecretsManager(configurator: options =>
+{
+    options.SecretFilter = entry => entry.Name.StartsWith($"{env}_{appName}");
+    options.KeyGenerator = (_, s) => s
+        .Replace($"{env}_{appName}_", string.Empty)
+        .Replace("__", ";");
+});
