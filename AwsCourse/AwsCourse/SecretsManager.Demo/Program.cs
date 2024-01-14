b03464904eda068da1ce071785dc96d420c65e94
donatellony <donatellony4736@gmail.com>
2024-01-14 17:29:57 +0100

    Added the Secrets Manager demo

---
+++ AwsCourse/AwsCourse/SecretsManager.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+var secretsManager = new AmazonSecretsManagerClient();
+
+var request = new GetSecretValueRequest
+{
+    SecretId = "ApiKey"
+};
+
+var response = await secretsManager.GetSecretValueAsync(request);
+
+Console.WriteLine(response.SecretString);
+
+var describeSecretRequest = new DescribeSecretRequest
+{
+    SecretId = "ApiKey",
+};
+var describeResponse = await secretsManager.DescribeSecretAsync(describeSecretRequest);
+
+Console.WriteLine();
