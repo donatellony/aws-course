2fcd5da9147d3952c092e7ba37754dedf12d7667
donatellony <donatellony4736@gmail.com>
2024-01-14 18:13:59 +0100

    Added the first C# lambda

---
+++ AwsCourse/AwsCourse/SimpleLambda/src/SimpleLambda/Function.cs
@@ -0,0 +1,14 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace SimpleLambda;
+
+public class Function
+{
+    public void FunctionHandler(ILambdaContext context)
+    {
+        context.Logger.LogInformation("Hello from C#!");
+    }
+}
