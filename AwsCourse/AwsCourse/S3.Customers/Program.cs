96cc26bcf1d695c1b64338f4e6c9e6a0758fd190
donatellony <donatellony4736@gmail.com>
2024-01-14 15:43:16 +0100

    Added the demo S3 implementation

---
+++ AwsCourse/AwsCourse/S3.Customers/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+
+// Just a VERY quick demo, the real project shouldn't be built like that
+const string filePath = "./example.jpeg";
+const string bucketName = "test-bucket-name";
+
+var s3Client = new AmazonS3Client();
+await using var inputStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+var s3Path = $"images/{Path.GetFileName(filePath)}";
+var putObjectRequest = new PutObjectRequest
+{
+    BucketName = bucketName,
+    Key = s3Path,
+    ContentType = "image/jpeg",
+    InputStream = inputStream
+};
+
+await s3Client.PutObjectAsync(putObjectRequest);
